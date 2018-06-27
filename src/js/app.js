3f59b4d1bf4bb6f84cbe5faf0a281bcf9aa872d4
wangtao <1621829316@qq.com>
2018-06-27 12:35:51 +0800

    完成了保存功能

--- src/js/app.js
+++ src/js/app.js
@@ -10,11 +10,41 @@ var app = new Vue({
       jobTitle: '前端工程师',
       phone: '13811111111',
       email: 'example@example.com'
+    },
+    signUpData:{
+      email:'',
+      password: ''
+    },
+    signInData:{
+      email:'',
+      password: ''
     }
   },
   methods: {
     onEdit(key, value){
       this.resume[key] = value
+    },
+    signUp(){
+      let user = new AV.User();
+      user.setUsername(this.signUpData.email);
+      user.setPassword(this.signUpData.password);
+      user.setEmail(this.signUpData.email)
+      user.signUp().then((loggedInUser)=> {
+      }, function (error) {
+      })
+    },
+    signIn(){
+      AV.User.logIn(this.signInData.email, this.signInData.password).then(function (loggedInUser) {
+      }, function (error) {
+        if(error.code === 211) {
+          alert('邮箱不存在')
+        } else if(error.code == 210) {
+          alert('邮箱和密码不破配')
+        }
+      })
+    },
+    signOut(){
+      AV.User.logOut()
     },
     save(){
       var currentUser = AV.User.current()
@@ -25,8 +55,11 @@ var app = new Vue({
         this.saveResume()
       }
     },
-    saveResume(){
-
+    saveResume() {
+      let {id} = AV.User.current()
+      var user = AV.Object.createWithoutData('User', id);
+      user.set('resume', this.resume);
+      user.save();
     }
   }
 })
