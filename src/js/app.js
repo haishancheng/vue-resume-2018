80aaa8097a51417a75446be97fa7928171d3bdfc
wangtao <1621829316@qq.com>
2018-06-27 14:48:27 +0800

    解决了登录注册保存的bug

--- src/js/app.js
+++ src/js/app.js
@@ -4,49 +4,60 @@ var app = new Vue({
     signInVisible: false,
     signUpVisible: false,
     resume: {
-      name:'SeaMountCity',
-      gender:'男',
-      birthday: '1993-10-10',
-      jobTitle: '前端工程师',
-      phone: '13811111111',
-      email: 'example@example.com'
+      name: '开始模板',
+      gender: '开始模板',
+      birthday: '开始模板',
+      jobTitle: '开始模板',
+      phone: '开始模板',
+      email: '开始模板'
     },
-    signUpData:{
-      email:'',
+    signUpData: {
+      email: '',
       password: ''
     },
-    signInData:{
-      email:'',
+    signInData: {
+      email: '',
       password: ''
     }
   },
   methods: {
-    onEdit(key, value){
+    onEdit(key, value) {
       this.resume[key] = value
     },
-    signUp(){
-      let user = new AV.User();
-      user.setUsername(this.signUpData.email);
-      user.setPassword(this.signUpData.password);
+    onSignUp() {
+      let user = new AV.User()
+      user.setUsername(this.signUpData.email)
+      user.setPassword(this.signUpData.password)
       user.setEmail(this.signUpData.email)
-      user.signUp().then((loggedInUser)=> {
+      user.signUp().then((loggedInUser) => {
+        this.signUpVisible = false
+        alert('注册成功')
+        this.getResume(loggedInUser.id)
       }, function (error) {
+        alert(error.rawMessage)
+        // if (error.code === 125) {
+        //   alert('请输入正确的邮箱地址')
+        // }
       })
     },
-    signIn(){
-      AV.User.logIn(this.signInData.email, this.signInData.password).then(function (loggedInUser) {
-      }, function (error) {
-        if(error.code === 211) {
+    onSignIn() {
+      AV.User.logIn(this.signInData.email, this.signInData.password).then((loggedInUser) => {
+        this.signInVisible = false
+        this.getResume(loggedInUser.id)
+      }, (error) => {
+        if (error.code === 211) {
           alert('邮箱不存在')
-        } else if(error.code == 210) {
+        } else if (error.code == 210) {
           alert('邮箱和密码不破配')
         }
       })
     },
-    signOut(){
+    onSignOut() {
       AV.User.logOut()
+      alert('注销成功')
+      this.resume = {name: '注销成功', gender: '注销成功', birthday: '注销成功', jobTitle: '注销成功', phone: '注销成功', email: '注销成功'}
     },
-    save(){
+    onSave() {
       var currentUser = AV.User.current()
       if (!currentUser) {
         this.signInVisible = true
@@ -57,12 +68,35 @@ var app = new Vue({
     },
     saveResume() {
       let {id} = AV.User.current()
-      var user = AV.Object.createWithoutData('User', id);
-      user.set('resume', this.resume);
-      user.save();
+      var user = AV.Object.createWithoutData('User', id)
+      user.set('resume', this.resume)
+      user.save().then(() => {
+        alert('保存成功')
+      }, () => {
+        alert('保存失败')
+      })
+    },
+    getResume(id) {
+      var user = new AV.Query('User')
+      user.get(id).then((newUser) => {
+        this.resume = newUser.attributes.resume || {name: '姓名', gender: '性别', birthday: '出生年月', jobTitle: '职位', phone: '电话', email: '邮箱'}
+      }, (error) => {
+      })
     }
   }
 })
+/* ☆☆☆☆☆☆
+ * 1.toJSON用法：
+ *   var obj = {name: '1', toJSON: function(){return {hello: 'hello'}}}
+ *   JSON.stringify(obj) ===> "{"hello":"hello"}"
+ * 2.VUE会和JSON.stringify一样返回toJSON中的值，所以在html中{{AV.User.current()}}会和console.log(AV.User.current())
+ *   显示的不一样，html中的{{AV.User.current()}}只会显示toJSON中返回的属性，比如它显示的是ObjectId，而console.log(AV.User.current())中显示的是id
+ */
+console.log(AV.User.current())
+if (AV.User.current()) {
+  //不需要app.methods.getResume，Vue会自动复制一份到上层
+  app.getResume(AV.User.current().id)
+}
 
 
 
