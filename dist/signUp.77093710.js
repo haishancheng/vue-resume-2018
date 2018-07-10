c50bc62bfbfc056f6eb719083b1bb9720b798075
wangtao <1621829316@qq.com>
2018-07-10 15:10:38 +0800

    parcel 打包

---
+++ dist/signUp.77093710.js
@@ -0,0 +1,336 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+// eslint-disable-next-line no-global-assign
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  return newRequire;
+})({18:[function(require,module,exports) {
+window.SignUp = {
+  template: '\n    <div class="signUp">\n      <form @submit.prevent="onSignUp" v-cloak>\n        <h2>\u7B80\u5386\u7F16\u8F91\u5668</h2>\n        <p>New jobs, new lives</p>\n        <router-link class="close" to="/">x</router-link>\n        <div class="row">\n          <label>\u90AE\u7BB1</label>\n          <input placeholder="example@example.com" type="text" v-model="signUpData.email" autocomplete="on">\n        </div>\n        <div class="row">\n          <label>\u5BC6\u7801</label>\n          <input placeholder="Enter password" type="password" v-model="signUpData.password" autocomplete="on">\n        </div>\n        <div class="row">\n          <label>\u786E\u8BA4\u5BC6\u7801</label>\n          <input placeholder="Confirm your password" type="password" v-model="signUpData.confirm" autocomplete="on">\n        </div>\n        <div class="actions">\n          <button type="submit">\u6CE8\u518C</button>\n          <p>\u672C\u7F51\u7AD9\u6682\u4E0D\u652F\u6301\u627E\u56DE\u5BC6\u7801\u7684\u529F\u80FD\uFF0C\u8BF7\u7262\u8BB0\u60A8\u7684\u8D26\u53F7\u548C\u5BC6\u7801\u54E6</p>\n        </div>\n      </form>\n      <div class="toSignUp">\n        \u5DF2\u7ECF\u6709\u8D26\u6237\u4E86\uFF1F\n        <router-link to="/signIn">\u767B\u5F55</router-link>\n      </div>\n      <transition name="bounce">\n        <prompt v-show="promptVisible" v-bind:prompt="prompt" @close-prompt="promptVisible = false"></prompt>\n      </transition>\n    </div>\n  ',
+  data: function data() {
+    return {
+      promptVisible: false,
+      signUpData: {
+        email: '',
+        password: '',
+        confirm: ''
+      },
+      prompt: {
+        icon: '',
+        info: ''
+      }
+    };
+  },
+  methods: {
+    onSignUp: function onSignUp() {
+      var _this = this;
+
+      if (!this.check()) {
+        return;
+      }
+      var user = new AV.User();
+      user.setUsername(this.signUpData.email);
+      user.setPassword(this.signUpData.password);
+      user.setEmail(this.signUpData.email);
+      user.signUp().then(function (loggedInUser) {
+        _this.$router.push({ name: '/', params: { message: 'signUpSuccess', email: _this.signUpData.email, password: _this.signUpData.password } });
+
+        // this.$router.push({name:'/', params:{message:'signUpSuccess',userID:loggedInUser.id}})
+      }, function (error) {
+        if (error.code === 125) {
+          _this.showPrompt('!', '请输入正确的邮箱地址！');
+        } else if (error.code === 203) {
+          _this.showPrompt('✘', '此电子邮箱已经被占用！');
+        }
+      });
+    },
+    check: function check() {
+      if (this.signUpData.email === '') {
+        this.showPrompt('!', '邮箱不能为空！');
+        return false;
+      } else if (this.signUpData.password === '') {
+        this.showPrompt('!', '密码不能为空！');
+        return false;
+      } else if (this.signUpData.confirm === '') {
+        this.showPrompt('!', '请确认密码！');
+        return false;
+      } else if (this.signUpData.password !== this.signUpData.confirm) {
+        this.showPrompt('!', '请确保两次输入的密码一致！');
+        return false;
+      }
+      return true;
+    },
+    showPrompt: function showPrompt(icon, info) {
+      this.prompt.icon = icon;
+      this.prompt.info = info;
+      this.promptVisible = true;
+    }
+  }
+};
+Vue.component('signUp', window.signUp);
+},{}],30:[function(require,module,exports) {
+var global = arguments[3];
+var OVERLAY_ID = '__parcel__error__overlay__';
+
+var OldModule = module.bundle.Module;
+
+function Module(moduleName) {
+  OldModule.call(this, moduleName);
+  this.hot = {
+    data: module.bundle.hotData,
+    _acceptCallbacks: [],
+    _disposeCallbacks: [],
+    accept: function (fn) {
+      this._acceptCallbacks.push(fn || function () {});
+    },
+    dispose: function (fn) {
+      this._disposeCallbacks.push(fn);
+    }
+  };
+
+  module.bundle.hotData = null;
+}
+
+module.bundle.Module = Module;
+
+var parent = module.bundle.parent;
+if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
+  var hostname = '' || location.hostname;
+  var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51606' + '/');
+  ws.onmessage = function (event) {
+    var data = JSON.parse(event.data);
+
+    if (data.type === 'update') {
+      data.assets.forEach(function (asset) {
+        hmrApply(global.parcelRequire, asset);
+      });
+
+      data.assets.forEach(function (asset) {
+        if (!asset.isNew) {
+          hmrAccept(global.parcelRequire, asset.id);
+        }
+      });
+      // Clear the console after HMR
+      console.clear();
+    }
+
+    if (data.type === 'reload') {
+      ws.close();
+      ws.onclose = function () {
+        location.reload();
+      };
+    }
+
+    if (data.type === 'error-resolved') {
+      console.log('[parcel] ✨ Error resolved');
+
+      removeErrorOverlay();
+    }
+
+    if (data.type === 'error') {
+      console.error('[parcel] 🚨  ' + data.error.message + '\n' + data.error.stack);
+
+      removeErrorOverlay();
+
+      var overlay = createErrorOverlay(data);
+      document.body.appendChild(overlay);
+    }
+  };
+}
+
+function removeErrorOverlay() {
+  var overlay = document.getElementById(OVERLAY_ID);
+  if (overlay) {
+    overlay.remove();
+  }
+}
+
+function createErrorOverlay(data) {
+  var overlay = document.createElement('div');
+  overlay.id = OVERLAY_ID;
+
+  // html encode message and stack trace
+  var message = document.createElement('div');
+  var stackTrace = document.createElement('pre');
+  message.innerText = data.error.message;
+  stackTrace.innerText = data.error.stack;
+
+  overlay.innerHTML = '<div style="background: black; font-size: 16px; color: white; position: fixed; height: 100%; width: 100%; top: 0px; left: 0px; padding: 30px; opacity: 0.85; font-family: Menlo, Consolas, monospace; z-index: 9999;">' + '<span style="background: red; padding: 2px 4px; border-radius: 2px;">ERROR</span>' + '<span style="top: 2px; margin-left: 5px; position: relative;">🚨</span>' + '<div style="font-size: 18px; font-weight: bold; margin-top: 20px;">' + message.innerHTML + '</div>' + '<pre>' + stackTrace.innerHTML + '</pre>' + '</div>';
+
+  return overlay;
+}
+
+function getParents(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return [];
+  }
+
+  var parents = [];
+  var k, d, dep;
+
+  for (k in modules) {
+    for (d in modules[k][1]) {
+      dep = modules[k][1][d];
+      if (dep === id || Array.isArray(dep) && dep[dep.length - 1] === id) {
+        parents.push(+k);
+      }
+    }
+  }
+
+  if (bundle.parent) {
+    parents = parents.concat(getParents(bundle.parent, id));
+  }
+
+  return parents;
+}
+
+function hmrApply(bundle, asset) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (modules[asset.id] || !bundle.parent) {
+    var fn = new Function('require', 'module', 'exports', asset.generated.js);
+    asset.isNew = !modules[asset.id];
+    modules[asset.id] = [fn, asset.deps];
+  } else if (bundle.parent) {
+    hmrApply(bundle.parent, asset);
+  }
+}
+
+function hmrAccept(bundle, id) {
+  var modules = bundle.modules;
+  if (!modules) {
+    return;
+  }
+
+  if (!modules[id] && bundle.parent) {
+    return hmrAccept(bundle.parent, id);
+  }
+
+  var cached = bundle.cache[id];
+  bundle.hotData = {};
+  if (cached) {
+    cached.hot.data = bundle.hotData;
+  }
+
+  if (cached && cached.hot && cached.hot._disposeCallbacks.length) {
+    cached.hot._disposeCallbacks.forEach(function (cb) {
+      cb(bundle.hotData);
+    });
+  }
+
+  delete bundle.cache[id];
+  bundle(id);
+
+  cached = bundle.cache[id];
+  if (cached && cached.hot && cached.hot._acceptCallbacks.length) {
+    cached.hot._acceptCallbacks.forEach(function (cb) {
+      cb();
+    });
+    return true;
+  }
+
+  return getParents(global.parcelRequire, id).some(function (id) {
+    return hmrAccept(global.parcelRequire, id);
+  });
+}
+},{}]},{},[30,18], null)
+//# sourceMappingURL=/signUp.77093710.map
